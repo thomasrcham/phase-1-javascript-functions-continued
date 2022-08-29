248682d9688b44bf663de5a6288cdc0fd4ebb845
thomasrcham <guseldorph@gmail.com>
2022-08-29 11:34:56 -0500

    commit tree e1541928cf9a17b07d4f53cf581828566d85544c

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+let sentence = "";
+
+
+function saturdayFun (activity = "roller-skate"){
+    sentence = (`This Saturday, I want to ${activity}!`);
+    return sentence;
+}
+
+function mondayWork (activity = "go to the office"){
+    sentence = (`This Monday, I will ${activity}.`);
+    return sentence;
+}
+
+function wrapAdjective (symbol){
+    if (symbol === "*"){
+    return function (){
+        let sentence = `You are ${symbol}a hard worker${symbol}!`
+        return sentence;}
+    } else {
+        return function (){
+            let sentence = `You are ${symbol}a dedicated programmer${symbol}!`
+            return sentence;}
+    }
+}
